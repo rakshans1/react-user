ec950e5d82ba7b5e35ec897fa2d880550837086e
Rakshan Shetty <shetty.raxx555@gmail.com>
2018-06-22 00:04:14 +0530

    Config change

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -7,6 +7,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import Loading from '../components/Loading';
 import {auth} from '../services';
