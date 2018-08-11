b60a62cef101e492b21bdfe4ae6989bc88215bcf
Rakshan Shetty <shetty.raxx555@gmail.com>
2018-08-11 23:10:21 +0530

    Enabled Redux devtools in prod

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -2,5 +2,11 @@ import {createStore} from 'redux';
 import reducers from '../reducers';
 
 export default function() {
+  const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+  const enhancers = [reduxDevtools];
+
+  const composedEnhancers = compose(...enhancers);
+
   return createStore(reducers);
 }
