2d107fa8d671f345090b9c9e1878396b4bfede5a
Rakshan Shetty <shetty.raxx555@gmail.com>
2018-08-11 23:49:18 +0530

    Enabled Redux devtools in prod

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -8,15 +8,15 @@ export default function() {
   const middlewares = [logger];
   const middlewareEnhancers = applyMiddleware(...middlewares);
 
-  const reduxDEvtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+  const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-  const enhancers = [middlewareEnhancers, monitorReducerEnhancer, reduxDEvtools];
+  const enhancers = [middlewareEnhancers, monitorReducerEnhancer, reduxDevtools];
   const composedEnhancers = compose(...enhancers);
 
   const store = createStore(reducers, undefined, composedEnhancers);
 
   if (module.hot) {
-    module.hot.accept('../reducers', () => 
+    module.hot.accept('../reducers', () =>
       store.replaceReducer(reducers)
     )
   }
